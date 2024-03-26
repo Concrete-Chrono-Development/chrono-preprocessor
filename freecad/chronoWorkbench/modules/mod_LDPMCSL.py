f4fac628eb9c924bf4fa542465d56a1e93e57f71
Matthew Troemner <31108019+mtroemner@users.noreply.github.com>
2024-03-26 11:48:05 -0400

    Add creation of a parameter file with every generation

--- freecad/chronoWorkbench/modules/mod_LDPMCSL.py
+++ freecad/chronoWorkbench/modules/mod_LDPMCSL.py
@@ -80,6 +80,7 @@ from freecad.chronoWorkbench.output.mkData_LDPMCSL_tets                   import
 from freecad.chronoWorkbench.output.mkData_LDPMCSL_facets                 import mkData_LDPMCSL_facets
 from freecad.chronoWorkbench.output.mkData_LDPMCSL_facetsVertices         import mkData_LDPMCSL_facetsVertices
 from freecad.chronoWorkbench.output.mkData_LDPMCSL_particles              import mkData_LDPMCSL_particles
+from freecad.chronoWorkbench.output.mkParameters                          import mkParameters
 
 
 
@@ -150,15 +151,23 @@ class inputWindow_LDPMCSL:
 
     def generationDriver(self):
 
-        fastGen = False
+        # Make a temporary path location
+        tempPath = tempfile.gettempdir() + "/chronoConc" + str(int(np.random.uniform(1e7,1e8))) + '/'
+        os.mkdir(tempPath)
 
-        driver_LDPMCSL(self,fastGen)
+        fastGen = False
+        mkParameters(self,tempPath)
+        driver_LDPMCSL(self,fastGen,tempPath)
 
     def generationDriverFast(self):
 
-        fastGen = True
+        # Make a temporary path location
+        tempPath = tempfile.gettempdir() + "/chronoConc" + str(int(np.random.uniform(1e7,1e8))) + '/'
+        os.mkdir(tempPath)
 
-        driver_LDPMCSL(self,fastGen)
+        fastGen = True
+        mkParameters(self,tempPath)
+        driver_LDPMCSL(self,fastGen,tempPath)
 
 
     def getStandardButtons(self):
@@ -267,103 +276,7 @@ class inputWindow_LDPMCSL:
 
     def writeParameters(self):
 
-        # Read in inputs from input panel
-        [setupFile, constitutiveEQ, matParaSet, \
-            numCPU, numIncrements,maxIter,placementAlg,\
-            geoType, dimensions, cadFile,\
-            minPar, maxPar, fullerCoef, sieveCurveDiameter, sieveCurvePassing,\
-            wcRatio, densityWater, cementC, flyashC, silicaC, scmC,\
-            cementDensity, flyashDensity, silicaDensity, scmDensity, airFrac1, \
-            fillerC, fillerDensity, airFrac2,\
-            htcToggle, htcLength,\
-            multiMatToggle,multiMatFile,multiMatRule,\
-            grainAggMin, grainAggMax, grainAggFuller, grainAggSieveD, grainAggSieveP,\
-            grainITZMin, grainITZMax, grainITZFuller, grainITZSieveD, grainITZSieveP,\
-            grainBinderMin, grainBinderMax, grainBinderFuller, grainBinderSieveD, grainBinderSieveP,\
-            outputDir, singleTetGen, modelType] = read_LDPMCSL_inputs(self.form)
-
-
-        # Make output directory if does not exist
-        outDir =  self.form[5].outputDir.text()
-        try:
-            os.mkdir(outDir)
-        except:
-            pass
-
-        # Write parameters to file
-        with open(Path(outDir + "/chronoWorkbench.cwPar"), "w") as f:
-            f.write("""
-// ================================================================================
-// CHRONO WORKBENCH - github.com/Concrete-Chrono-Development/chrono-preprocessor
-//
-// Copyright (c) 2023 
-// All rights reserved. 
-//
-// Use of the code that generated this file is governed by a BSD-style license that
-// can be found in the LICENSE file at the top level of the distribution and at
-// github.com/Concrete-Chrono-Development/chrono-preprocessor/blob/main/LICENSE
-//
-// ================================================================================
-// Chrono Workbench Parameter File
-// ================================================================================
-// 
-// Chrono Workbench developed by Northwestern University
-//
-// ================================================================================
-            \n\n""")
-            f.write("constitutiveEQ = " + constitutiveEQ + "\n")
-            f.write("matParaSet = " + matParaSet + "\n")
-            f.write("numCPU = " + str(numCPU) + "\n")
-            f.write("numIncrements = " + str(numIncrements) + "\n")
-            f.write("maxIter = " + str(maxIter) + "\n")
-            f.write("placementAlg = " + placementAlg + "\n")
-            f.write("geoType = " + geoType + "\n")
-            f.write("dimensions = " + str(dimensions) + "\n")
-            f.write("cadFile = " + cadFile + "\n")
-            f.write("minPar = " + str(minPar) + "\n")
-            f.write("maxPar = " + str(maxPar) + "\n")
-            f.write("fullerCoef = " + str(fullerCoef) + "\n")
-            f.write("sieveCurveDiameter = " + str(sieveCurveDiameter) + "\n")
-            f.write("sieveCurvePassing = " + str(sieveCurvePassing) + "\n")
-            f.write("wcRatio = " + str(wcRatio) + "\n")
-            f.write("densityWater = " + str(densityWater) + "\n")
-            f.write("cementC = " + str(cementC) + "\n")
-            f.write("flyashC = " + str(flyashC) + "\n")
-            f.write("silicaC = " + str(silicaC) + "\n")
-            f.write("scmC = " + str(scmC) + "\n")
-            f.write("cementDensity = " + str(cementDensity) + "\n")
-            f.write("flyashDensity = " + str(flyashDensity) + "\n")
-            f.write("silicaDensity = " + str(silicaDensity) + "\n")
-            f.write("scmDensity = " + str(scmDensity) + "\n")
-            f.write("airFrac1 = " + str(airFrac1) + "\n")
-            f.write("fillerC = " + str(fillerC) + "\n")
-            f.write("fillerDensity = " + str(fillerDensity) + "\n")
-            f.write("airFrac2 = " + str(airFrac2) + "\n")
-            f.write("htcToggle = " + htcToggle + "\n")
-            f.write("htcLength = " + str(htcLength) + "\n")
-            f.write("multiMatToggle = " + multiMatToggle + "\n")
-            f.write("multiMatFile = " + multiMatFile + "\n")
-            f.write("multiMatRule = " + str(multiMatRule) + "\n")
-            f.write("grainAggMin = " + str(grainAggMin) + "\n")
-            f.write("grainAggMax = " + str(grainAggMax) + "\n")
-            f.write("grainAggFuller = " + str(grainAggFuller) + "\n")
-            f.write("grainAggSieveD = " + str(grainAggSieveD) + "\n")
-            f.write("grainAggSieveP = " + str(grainAggSieveP) + "\n")
-            f.write("grainITZMin = " + str(grainITZMin) + "\n")
-            f.write("grainITZMax = " + str(grainITZMax) + "\n")
-            f.write("grainITZFuller = " + str(grainITZFuller) + "\n")
-            f.write("grainITZSieveD = " + str(grainITZSieveD) + "\n")
-            f.write("grainITZSieveP = " + str(grainITZSieveP) + "\n")
-            f.write("grainBinderMin = " + str(grainBinderMin) + "\n")
-            f.write("grainBinderMax = " + str(grainBinderMax) + "\n")
-            f.write("grainBinderFuller = " + str(grainBinderFuller) + "\n")
-            f.write("grainBinderSieveD = " + str(grainBinderSieveD) + "\n")
-            f.write("grainBinderSieveP = " + str(grainBinderSieveP) + "\n")
-            f.write("singleTetGen = " + str(singleTetGen) + "\n")
-            f.write("modelType = " + modelType + "\n")
-            f.write("outputDir = " + outputDir + "\n")
-
-        print("Parameters written to file")
+        mkParameters(self,"writeOnly")
 
     def readParameters(self):
 
